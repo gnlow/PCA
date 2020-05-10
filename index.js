6bdd82c233e4e80cf144259002ca31e0a3af44ad
Gnlow <entrydark@gmail.com>
2020-05-10 15:57:41 +0900

    나이별로 분리해 분석

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const {PCA} = require("ml-pca")
 const gaussian = require("gaussian")
 const dataset = require("@randkid/size")
-const d = dataset.f_dropna.data
-const pca = new PCA(d)
-const distributions = 
-    pca.getStandardDeviations()
-        .map(σ => gaussian(0, σ**2))
-console.log( pca.invert([distributions.map(d => d.ppf(Math.random()))]) )
+const {groupBy} = require("lodash")
+
+const ds = groupBy( dataset.f_dropna.data, 0 )
+for(const k in ds){
+    const pca = new PCA(ds[k])
+    console.log(pca.getStandardDeviations()[0])
+    const distributions = 
+        pca.getStandardDeviations()
+            .splice(0, 5)
+            .map(σ => gaussian(0, σ**2))
+    console.log( pca.invert([distributions.map(d => d.ppf(Math.random()))]) )
+}
